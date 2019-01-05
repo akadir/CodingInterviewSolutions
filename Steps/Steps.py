26384b778e67547fdb8cd1004fe1131dc9490fe5
kadir <akadirkkoc@gmail.com>
2019-01-05 17:51:11 +0300

    Add steps solutions

---
+++ Steps/Steps.py
@@ -0,0 +1,84 @@
+import sys
+import unittest
+from io import StringIO
+
+
+def first_solution(number):
+    if number < 0:
+        return
+    else:
+        for i in range(1, number + 1):
+            for j in range(0, number):
+                if j < i:
+                    print('#', end='')
+                else:
+                    print(' ', end='')
+            print()
+
+
+def second_solution(number):
+    for row in range(0, number):
+        stair = ''
+        for column in range(0, number):
+            if column <= row:
+                stair += '#'
+            else:
+                stair += ' '
+        print(stair)
+
+
+def third_solution(number, row=0, stair=''):
+    if number == row:
+        return
+
+    if number == len(stair):
+        print(stair)
+        return third_solution(number, row + 1)
+
+    if len(stair) <= row:
+        stair += '#'
+    else:
+        stair += ' '
+
+    third_solution(number, row, stair)
+
+
+class StepsTest(unittest.TestCase):
+    def setUp(self):
+        self.held, sys.stdout = sys.stdout, StringIO()
+
+    def test_call_first_solution_with_one(self):
+        first_solution(1)
+        self.assertEqual(sys.stdout.getvalue(), '#\n')
+
+    def test_call_first_solution_with_two(self):
+        first_solution(2)
+        self.assertEqual(sys.stdout.getvalue(), '# \n##\n')
+
+    def test_call_first_solution_with_three(self):
+        first_solution(3)
+        self.assertEqual(sys.stdout.getvalue(), '#  \n## \n###\n')
+
+    def test_call_second_solution_with_one(self):
+        second_solution(1)
+        self.assertEqual(sys.stdout.getvalue(), '#\n')
+
+    def test_call_second_solution_with_two(self):
+        second_solution(2)
+        self.assertEqual(sys.stdout.getvalue(), '# \n##\n')
+
+    def test_call_second_solution_with_three(self):
+        second_solution(3)
+        self.assertEqual(sys.stdout.getvalue(), '#  \n## \n###\n')
+
+    def test_call_third_solution_with_one(self):
+        third_solution(1)
+        self.assertEqual(sys.stdout.getvalue(), '#\n')
+
+    def test_call_third_solution_with_two(self):
+        third_solution(2)
+        self.assertEqual(sys.stdout.getvalue(), '# \n##\n')
+
+    def test_call_third_solution_with_three(self):
+        third_solution(3)
+        self.assertEqual(sys.stdout.getvalue(), '#  \n## \n###\n')
