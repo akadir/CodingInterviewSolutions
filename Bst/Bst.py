d373f6085410e8b0c2ba8edc4f1c729d70b5cea1
kadir <akadirkkoc@gmail.com>
2019-01-12 18:37:03 +0300

    Implement contains method for binary search tree

--- Bst/Bst.py
+++ Bst/Bst.py
@@ -17,6 +17,16 @@ class Node:
         elif data >= self.data:
             self.right = Node(data)
 
+    def contains(self, data):
+        if self.data is data:
+            return self
+        if data < self.data and self.left is not None:
+            return self.left.contains(data)
+        elif data > self.data and self.right is not None:
+            return self.right.contains(data)
+        else:
+            return None
+
 
 class BstTest(unittest.TestCase):
     def test_insert(self):
@@ -28,3 +38,26 @@ class BstTest(unittest.TestCase):
         self.assertEqual(5, node.left.data)
         self.assertEqual(15, node.right.data)
         self.assertEqual(17, node.right.right.data)
+
+    def test_contains(self):
+        node = Node(10)
+        node.insert(5)
+        node.insert(15)
+        node.insert(20)
+        node.insert(0)
+        node.insert(-5)
+        node.insert(3)
+
+        three = node.left.left.right
+        self.assertEqual(three, node.contains(3))
+
+    def test_should_return_none(self):
+        node = Node(10)
+        node.insert(5)
+        node.insert(15)
+        node.insert(20)
+        node.insert(0)
+        node.insert(-5)
+        node.insert(3)
+
+        self.assertEqual(None, node.contains(9999))
