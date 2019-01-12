41a94d158a4c523b1e0741df7642c3a8be18bcbf
kadir <akadirkkoc@gmail.com>
2019-01-12 17:04:57 +0300

    Implement remove method for tree node

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -10,6 +10,12 @@ class Node:
         node = Node(data)
         self.children.append(node)
 
+    def remove(self, data):
+        self.children = [n for n in self.children if n.data is not data]
+        # for d in self.children:
+        #     if d.data is data:
+        #         self.children.remove(d)
+
 
 class TreeTest(unittest.TestCase):
     def test_create_node(self):
@@ -22,3 +28,10 @@ class TreeTest(unittest.TestCase):
         n.add('b')
         self.assertEqual(1, len(n.children))
         self.assertEqual([], n.children[0].children)
+
+    def test_remove_node(self):
+        n = Node('a')
+        n.add('b')
+        self.assertEqual(1, len(n.children))
+        n.remove('b')
+        self.assertEqual(0, len(n.children))
